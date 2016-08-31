2e23388222982c3f612e7a197f7d84f868249990
David Wales <davidwales2@gmail.com>
2016-08-31 11:16:51 +1000

    Add pretty print and maximum size for array to reduce memory usage

--- countdown.go
+++ countdown.go
@@ -1,6 +1,9 @@
 package countdown
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type Countdown struct {
 	times        []int64
@@ -8,6 +11,7 @@ type Countdown struct {
 	lastEstimate int64
 	count        int64
 	total        int64
+	maxTimes     int
 }
 
 func (c *Countdown) Start(length int) {
@@ -16,6 +20,19 @@ func (c *Countdown) Start(length int) {
 	c.lastEstimate = 0
 	c.count = int64(length)
 	c.lastCount = time.Now().UnixNano()
+	c.maxTimes = 64
+}
+
+func (c Countdown) PrettyPrint() string {
+	var timeLeft string
+	if c.SecondsRemaining() > 3599 {
+		timeLeft = fmt.Sprintf("%dh", c.SecondsRemaining()/3600)
+	} else if c.SecondsRemaining() > 59 {
+		timeLeft = fmt.Sprintf("%dm", c.SecondsRemaining()/60)
+	} else {
+		timeLeft = fmt.Sprintf("%ds", c.SecondsRemaining())
+	}
+	return fmt.Sprintf("%d%% complete - %s remaining", c.PercentageComplete(), timeLeft)
 }
 
 func (c Countdown) SecondsRemaining() int64 {
@@ -27,7 +44,11 @@ func (c Countdown) PercentageComplete() int {
 }
 
 func (c *Countdown) Count() {
-	c.times = append(c.times, time.Now().UnixNano()-c.lastCount)
+	if len(c.times) > c.maxTimes-1 {
+		c.times = append(c.times[1:], time.Now().UnixNano()-c.lastCount)
+	} else {
+		c.times = append(c.times, time.Now().UnixNano()-c.lastCount)
+	}
 	c.lastCount = time.Now().UnixNano()
 	c.count--
 	total := int64(0)
